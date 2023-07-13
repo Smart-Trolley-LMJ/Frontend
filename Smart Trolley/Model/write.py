958deab4e90ec86fe0e6e0efde11fbc3205febed
LorenzoAppiadu <92297186+LorenzoAppiadu@users.noreply.github.com>
2023-07-13 06:52:03 +0000

    Update write.py

--- Smart Trolley/Model/write.py
+++ Smart Trolley/Model/write.py
@@ -6,11 +6,12 @@ import json
 
 reader = SimpleMFRC522()
 
-with open("./json/image.json", "r") as file:
+with open("../json/image.json", "r") as file:
         data = json.load(file)
 
 for item in data:
     try:
+            item = json.dumps(item)
             text = input('New data:')
             print("Now place your tag to write")
             reader.write(item)
