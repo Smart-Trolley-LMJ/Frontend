733f1c8e4b46f14e846618858d954e04b926bb31
LorenzoAppiadu <92297186+LorenzoAppiadu@users.noreply.github.com>
2023-07-13 06:42:18 +0000

    Write to rfid tags

---
+++ Smart Trolley/Model/write.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import RPi.GPIO as GPIO
+from mfrc522 import SimpleMFRC522
+import json
+
+reader = SimpleMFRC522()
+
+with open("./json/image.json", "r") as file:
+        data = json.load(file)
+
+for item in data:
+    try:
+            text = input('New data:')
+            print("Now place your tag to write")
+            reader.write(item)
+            print("Written")
+    finally:
+            GPIO.cleanup()
