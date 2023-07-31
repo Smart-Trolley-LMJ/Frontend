3eba3e631162ce4a90b99aa61b9783139d321cdf
LorenzoAppiadu <92297186+LorenzoAppiadu@users.noreply.github.com>
2023-07-31 19:04:52 +0000

    QT Web Engine

--- Smart Trolley/test.py
+++ Smart Trolley/test.py
@@ -1,54 +1,18 @@
 import sys
-import threading
-from PyQt5.QtWidgets import QApplication, QDialog, QLabel, QVBoxLayout, QPushButton, QProgressBar
+from PyQt5.QtWidgets import QApplication, QMessageBox
+from PyQt5.QtCore import QUrl
+from PyQt5.QtGui import QDesktopServices
 
-class LoaderDialog(QDialog):
-    def __init__(self):
-        super().__init__()
-
-        self.setWindowTitle("Loading...")
-        self.setFixedSize(300, 100)
-
-        layout = QVBoxLayout(self)
-        self.progress_bar = QProgressBar(self)
-        layout.addWidget(QLabel("Please wait while loading..."))
-        layout.addWidget(self.progress_bar)
-        self.progress_bar.setRange(0, 0)  # Indeterminate progress bar
-
-        # Make the dialog modal to prevent interaction with the main window
-        self.setModal(True)
-
-class MainWindow(QDialog):
-    def __init__(self):
-        super().__init__()
-
-        self.setWindowTitle("Main Window")
-        self.setGeometry(100, 100, 400, 200)
-
-        layout = QVBoxLayout(self)
-        self.load_button = QPushButton("Load Data", self)
-        layout.addWidget(self.load_button)
-
-        self.load_button.clicked.connect(self.start_loading)
-
-    def start_loading(self):
-        loader_dialog = LoaderDialog()
-        loading_thread = threading.Thread(target=self.perform_request, args=(loader_dialog,))
-        loading_thread.start()
-
-        # Show the loader dialog while waiting for the thread to finish
-        loader_dialog.exec_()
-
-    def perform_request(self, loader_dialog):
-        # Simulate a backend request that takes some time
-        import time
-        time.sleep(5)
-
-        # The request is completed; close the loader dialog
-        loader_dialog.accept()
+def open_payment_link(payment_link):
+    # Try to open the payment link in the default web browser
+    if QDesktopServices.openUrl(QUrl(payment_link)):
+        print("Payment link opened successfully.")
+    else:
+        # If there is an error, show an error message to the user
+        QMessageBox.critical(None, "Error", "Failed to open the payment link.", QMessageBox.Ok)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    main_window = MainWindow()
-    main_window.show()
+    payment_link = "https://example.com/payment"  # Replace this with the actual payment link received from the backend
+    open_payment_link(payment_link)
     sys.exit(app.exec_())
