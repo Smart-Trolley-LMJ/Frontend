3ffd54f77028e0fa66bf3237b596cb5038f7f1f2
LorenzoAppiadu <92297186+LorenzoAppiadu@users.noreply.github.com>
2023-08-01 03:39:37 +0000

    Update

--- Smart Trolley/cart.py
+++ Smart Trolley/cart.py
@@ -75,11 +75,7 @@ class ShoppingCart():
     def read_RFID(self, worker: Worker):
         while True:
             self.id, text = self.reader.read()
-            print(f'TagID:{text} Type:{type(text)}')
-            try:
-                text.decode('uft-8')
-            except:
-                pass
+            text = text[:36]
             a = requests.get(f'{url}inventories/{text}').content.decode('utf-8')
             print(a)
             time.sleep(2)
